eb1b8393cb4e3bfb2b3e95767441c27629e255be
Luisfer593 <luisfercuji_@hotmail.com>
2025-04-14 19:40:40 -0500

    Server modificacion package.json2

--- server.js
+++ server.js
@@ -1,12 +1,12 @@
-// server.js
-import app from './app.js';
+import app from './app.js';  // Asegúrate de que la ruta sea correcta dependiendo de la ubicación de tu archivo app.js
 import cors from 'cors';
 
-const PORT = process.env.PORT || 4000;
+const PORT = process.env.PORT || 4000;  // Usamos el puerto de Vercel o 4000 por defecto
 
-// Habilitar CORS
+// Habilitar CORS para todas las rutas
 app.use(cors());
 
+// Iniciar el servidor
 app.listen(PORT, () => {
   console.log(`Servidor corriendo en http://localhost:${PORT}`);
 });
